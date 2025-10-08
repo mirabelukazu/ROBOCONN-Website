5a0e3d0766880f7a10aee3d0ad9200488c4e30e3
mirabelukazu <mirabel_ukazu@brown.edu>
2025-10-08 12:24:38 -0400

    RoboCONN

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ var scheduleTabNav = function(scheduleTabClick){
     scheduleTeamTabBtns.forEach((scheduleTeamTabBtn) => {
         scheduleTeamTabBtn.classList.remove("active");
     });
+
     scheduleTabContents[scheduleTabClick].style.display = "flex";
-    scheduleTabContents[scheduleTabClick].classList.add("active");
+
+    setTimeout(() => {
+        scheduleTabContents[scheduleTabClick].classList.add("active");
+    }, 100);
+    
     scheduleTeamTabBtns[scheduleTabClick].classList.add("active");
 }
 
@@ -22,5 +27,73 @@ scheduleTeamTabBtns.forEach((scheduleTeamTabBtn, i) => {
         scheduleTabNav(i);
     });
 });
+
 /*Team modals, tabs and cards*/
+//Filter team cards according to team tabs
+document.addEventListener("DOMContentLoaded", () => {
+    const teamTabs = document.querySelector(".team-tabs");
+    const teamTabsBtns = teamTabs.querySelectorAll(".tab-btn");
+    const cardwithModals = document.querySelectorAll(".team-container .card-with-modal");
+
+    teamTabsBtns.forEach((tabBtn) => {
+        tabBtn.addEventListener("click", () => {
+            const filter = tabBtn.getAttribute("data-filter");
+
+            cardwithModals.forEach((cardwithModal) => {
+                if(filter === "all" || cardwithModal.classList.contains(filter)){
+                    cardwithModal.classList.remove("hidden");
+
+                    setTimeout(() => {
+                        cardwithModal.style.opacity = "1";
+                        cardwithModal.style.transition = ".5s ease";
+                    }, 1);
+                }
+                else{
+                    cardwithModal.classList.add("hidden");
+
+                    setTimeout(() => {
+                        cardwithModal.style.opacity = "0";
+                        cardwithModal.style.transition = ".5s ease";
+                    }, 1);
+                }
+            });
+            //Add active class to the clicked tab btn
+            teamTabsBtns.forEach((tabBtn) => tabBtn.classList.remove("active"));
+            tabBtn.classList.add("active");
+        });
+    });
+});
+
+//Open/Close Team modals
+const teamCardsWithModals = document.querySelectorAll(".team-container .card-with-modal");
+
+teamCardsWithModals.forEach((teamCardWithModal) => {
+    const teamCard = teamCardWithModal.querySelector(".team-card");
+    const teamBackdrop = teamCardWithModal.querySelector(".team-modal-backdrop");
+    const teamModal = teamCardWithModal.querySelector(".team-modal");
+    const modalCloseBtn = teamCardWithModal.querySelector(".modal-close-btn");
+
+    teamCard.addEventListener("click", () => {
+        teamBackdrop.style.display = "flex";
+
+        setTimeout(() => {
+            teamBackdrop.classList.add("active");
+        }, 300);
+
+        setTimeout(() => {
+            teamModal.classList.add("active");
+        }, 300);
+    });
+
+    modalCloseBtn.addEventListener("click", () => { 
+        setTimeout(() => {
+            teamBackdrop.style.display = "none";
+        }, 500);
+
+        setTimeout(() => {
+            teamBackdrop.classList.remove("active");
+            teamModal.classList.remove("active");
+        }, 100);
+    });
+});
 /*Shrink the Height of the header on scroll*/
